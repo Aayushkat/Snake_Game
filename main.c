0683f3c66cf18a0e3356ea2cbd1c55e99f983ce4
Aayush <aayushkatoch5577@gmail.com>
2025-09-22 23:21:35 +0530

    Rectified the fill_board function
    Signed-off-by: Aayush <aayushkatoch5577@gmail.com>

--- main.c
+++ main.c
@@ -1,40 +1,44 @@
 #include <stdio.h>
+#include <conio.h>
 #define rows 20
 #define columns 20
 
-
 char board[rows * columns];
 
-void fill_board(){
-    int x,y;
-    for(y=0;y<rows;y++){
-        for(x=0;x<columns;x++){
-            if(x==0||y==0||x==columns-1||y==columns-1){
-                board[y*columns+x]='#';
-            }
-
-        }
-    }
-
-}
-
-void print_board(){
-        int x,y;
-    for(y=0;y<rows;y++){
-        for(x=0;x<columns;x++){
-            if(x==0||y==0||x==columns-1||y==columns-1){
-                putchar(board[y*columns+x]);
-            }
-            putchar('\n');
-
-        }
-    }
-
-}
-
-    int
-    main()
+void fill_board()
 {
+    int x, y;
+    for (y = 0; y < rows; y++)
+    {
+        for (x = 0; x < columns; x++)
+        {
+            if (x == 0 || y == 0 || x == columns - 1 || y == rows - 1)
+            {
+                board[y * columns + x] = '#';
+            }
+            else
+            board[y*columns+x]=' ';
+        }
+    }
+}
 
+void print_board()
+{
+    int x, y;
+    for (y = 0; y < rows; y++)
+    {
+        for (x = 0; x < columns; x++)
+        {
+
+            putchar(board[y * columns + x]);
+        }
+        putchar('\n');
+    }
+}
+
+int main()
+{
+    fill_board();
+    print_board();
     return 0;
 }
