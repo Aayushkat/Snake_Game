2a839cc5ecb6c5ac53ebedb57f1799d04430e138
Aayush <aayushkatoch5577@gmail.com>
2025-09-24 21:49:36 +0530

    complete the game
    Signed-off-by: Aayush <aayushkatoch5577@gmail.com>

--- main.c
+++ main.c
@@ -1,88 +1,173 @@
+// Copyright EightSprites.
+// Live coding available at: https://www.youtube.com/watch?v=t3y2b2_moY8
+// This file is put in public domain.
+// You can use it as you like.
+
 #include <stdio.h>
-#include <conio.h>
 #include <stdlib.h>
-#define SNAKE_MAX_LEN 256
-#define rows 20
-#define columns 20
+#include <conio.h>
+#include <time.h>
 
-char board[rows * columns];
-int snakeX=5,snakeY=5;
-int isGameover=0;
-void fill_board()
+#define cols 25
+#define rows 25
+#define foods 50
 
-{
-    int x, y;
-    for (y = 0; y < rows; y++)
-    {for (x = 0; x < columns; x++)
-        {if (x == 0 || y == 0 || x == columns - 1 || y == rows - 1)
-            { board[y * columns + x] = '#';
-            }
-            else
-            board[y*columns+x]=' ';//for space
-        }
-    }
-}
+char board[cols * rows];
 
-void clear_screen(){
-    system("cls");
-}
-void print_board()
-{
-    int x, y;
-    clear_screen();
-    for (y = 0; y < rows; y++)
-    {for (x = 0; x < columns; x++)
-        {
-            putchar(board[y * columns + x]);
-        }
-        putchar('\n');
-    }
-}
-struct snake_Part{
+int isGameOver = 0;
+
+void fill_board() {
     int x,y;
+    
+    for(y=0; y<rows; y++) {
+        for(x=0; x<cols; x++) {
+            if(x==0||y==0||x==cols-1||y==rows-1) {
+                board[y*cols + x] = '#';
+            }else{
+                board[y*cols + x] = ' ';
+            }
+        }
+    }
+}
+
+void clear_screen() {
+    // Hack clear screen on Windows. Do not use!
+    system("cls");
+    // on Linux & MAC
+    // system("clear");
+}
+
+void print_board() {
+    int x,y;
+        
+    for(y=0; y<rows; y++) {
+        for(x=0; x<cols; x++) {
+            putch(board[y*cols + x]);
+        }
+        putch('\n');
+    }    
+}
+
+#define SNAKE_MAX_LEN 256
+struct SnakePart {
+    int x, y;
 };
-struct Snake{
+struct Snake {
     int length;
-    struct snake_Part part[SNAKE_MAX_LEN];
+    struct SnakePart part[SNAKE_MAX_LEN];
 };
 struct Snake snake;
+
+struct Food {
+    int x, y;
+    int consumed;
+};
+struct Food food[foods];
+
 void draw_snake() {
     int i;
     
     for(i=snake.length-1; i>0; i--) {
-        board[snake.part[i].y*columns + snake.part[i].x] = '*';
+        board[snake.part[i].y*cols + snake.part[i].x] = '*';
     }
-    board[snake.part[0].y*columns + snake.part[0].x] = '0';
+    board[snake.part[0].y*cols + snake.part[0].x] = '@';
 }
 
+void move_snake(int deltaX, int deltaY) {
+    int i;
 
-void draw_snake(){
-    board[snakeX* columns + snakeY]='O';
-}
-
-void move_snake(int deltaX, int deltaY){
-    snakeX+=deltaX;
-    snakeY+=deltaY;
-}
-
-void read_keyboard(){
-int ch=getch();
-switch (ch){
-    case 'a':move_snake(0,-1);break;
-    case 'd':move_snake(0,1);break;
-    case 'w':move_snake(-1,0);break;
-    case 's':move_snake(1,0);break;
-}}
-
-int main()
-{
-    while(!isGameover){
-    snake_body();
-    fill_board();
-    draw_snake();
-    print_board();
-    read_keyboard();
+    for(i=snake.length-1; i>0; i--) {
+        snake.part[i] = snake.part[i-1];
+    }
     
+    snake.part[0].x += deltaX;
+    snake.part[0].y += deltaY;
 }
-    return 0;
+
+void read_keyboard() {
+    int ch = getch();
+    
+    switch(ch) {
+        case 'w': move_snake( 0,-1); break;
+        case 's': move_snake( 0, 1); break;
+        case 'a': move_snake(-1, 0); break;
+        case 'd': move_snake( 1, 0); break;        
+    }
+}
+
+void draw_food() {
+    int i;
+    
+    for(i=0; i<foods; i++) {
+        if( !food[i].consumed ) {
+            board[food[i].y*cols + food[i].x] = '+';
+        }
+    }
+}
+
+void setup_food() {
+    int i;
+    
+    for(i=0; i<foods; i++) {
+        food[i].x = 1 + rand() % (cols-2);
+        food[i].y = 1 + rand() % (rows-2);
+        food[i].consumed = 0;
+    }
+}
+
+void setup_snake() {
+    snake.length = 1;
+    snake.part[0].x = 1 + rand() % (cols-2);
+    snake.part[0].y = 1 + rand() % (rows-2);
+}
+
+void game_rules() {
+    int i;
+    
+    for(i=0; i<foods; i++) {
+        if( !food[i].consumed ) {
+            if( food[i].x == snake.part[0].x &&
+                food[i].y == snake.part[0].y ) {
+                    food[i].consumed = 1;
+                    snake.length++;
+            }
+        }
+    }
+    
+    if( snake.part[0].x == 0 || snake.part[0].x == cols-1 ||
+        snake.part[0].y == 0 || snake.part[0].y == rows-1 ) {
+            isGameOver = 1;
+    }
+    
+    for(i=1; i<snake.length; i++) {
+        if( snake.part[0].x == snake.part[i].x &&
+            snake.part[0].y == snake.part[i].y ) {
+                isGameOver = 1;
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{   
+    srand(time(0));
+    
+    setup_snake();
+    setup_food();
+    
+    while(!isGameOver) {
+        fill_board();
+        draw_food();
+        draw_snake();
+        game_rules();
+        clear_screen();
+        printf("Snake Game, Score: %d\n", snake.length * 100);
+        print_board();
+        if( !isGameOver ) read_keyboard();
+    }
+    
+    printf("Game Over, Final score: %d\n", snake.length * 100);
+    
+    while(1) getch();
+    
+	return 0;
 }
