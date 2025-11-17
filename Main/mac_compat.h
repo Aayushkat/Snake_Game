1b54f21af6791ec584b8f17616eb27b37a5b8daa
Pranshu640 <pranshubansal603@gmail.com>
2025-11-17 20:01:47 +0530

    Sleep-function OS Based Condition Added to resolve OS confliction edge case

--- Main/mac_compat.h
+++ Main/mac_compat.h
@@ -1,10 +1,19 @@
 #ifndef MAC_COMPAT_H
 #define MAC_COMPAT_H
 
+#ifdef __APPLE__
+
 // macOS compatibility layer for Windows-specific functions
-#include <unistd.h>
+#include <time.h>
 
 
-#define Sleep(ms) usleep((ms) * 1000)
+#define Sleep(ms) do { \
+    struct timespec ts; \
+    ts.tv_sec = (ms) / 1000; \
+    ts.tv_nsec = ((ms) % 1000) * 1000000; \
+    nanosleep(&ts, NULL); \
+} while (0)
+
+#endif // __APPLE__
 
 #endif // MAC_COMPAT_H
